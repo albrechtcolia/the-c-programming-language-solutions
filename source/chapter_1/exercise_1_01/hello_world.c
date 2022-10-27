6e50ee876c9630b48947bd084ca86495a34d125d
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-26 22:43:22 -0300

    Chapter 1

--- source/chapter_1/exercise_1_01/hello_world.c
+++ source/chapter_1/exercise_1_01/hello_world.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -13,3 +14,5 @@ int main (int argc, char *argv[])
 
     return 0;
 }
+
+
---
+++ source/chapter_1/exercise_1_03/fahrenheit-celsius.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+/**
+ * Exercise 1-03
+ * Modify the temperature conversion program to print a heading above the table.
+ */
+
+/**
+ * Fahrenheit-Celsius table.
+ * Floating point version.
+ */
+
+int main (int argc, char *argv[])
+{
+    float fahr, celsius;
+    int   lower, upper, step;
+
+    lower = 0;      // Lower limit.
+    upper = 300;    // Upper limit.
+    step  = 20;     // Step size.
+
+    printf("Fahrenheit\tCelsius\n");
+    printf("---------------------------\n");
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t\t%3.1f\n", fahr, celsius);
+        fahr += step;
+    }
+    
+    return 0;
+}
+
+
