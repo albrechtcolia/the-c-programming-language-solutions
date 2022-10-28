afdeeacb6198c0a7b28b9895863dea33b480ae09
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-27 22:58:30 -0300

    Chapter 1

--- source/chapter_1/exercise_1_03/fahrenheit-celsius.c
+++ source/chapter_1/exercise_1_03/fahrenheit-celsius.c
@@ -26,7 +26,7 @@ int main (int argc, char *argv[])
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f\t\t%3.1f\n", fahr, celsius);
+        printf("%3.0f\t\t%6.2f\n", fahr, celsius);
         fahr += step;
     }
     
