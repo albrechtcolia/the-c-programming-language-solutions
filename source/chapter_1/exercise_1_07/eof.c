2ace886a47b8be660ff0cc1eb0824c1cf25b06e1
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-30 00:10:01 -0300

    Chapter 1

---
+++ source/chapter_1/exercise_1_07/eof.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+ * Exercise 1-07
+ * Write a program to print the value of EOF.
+ */
+
+int main (int argc, char *argv[])
+{
+    printf("EOF value: %d\n", EOF);    
+
+    return 0;
+}
