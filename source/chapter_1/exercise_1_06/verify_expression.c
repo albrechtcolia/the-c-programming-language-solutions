7a898c050a80fe8ef00e272173bc71c691319de5
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-29 23:59:41 -0300

    Chapter 1

---
+++ source/chapter_1/exercise_1_06/verify_expression.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+ * Exercise 1-06
+ * Verify that the expression getchar() != EOF is 0 or 1.
+ */
+
+int main (int argc, char *argv[])
+{
+    printf("Value: %d", getchar() != EOF);
+
+    return 0;
+}
