95ec55dd84575bd10e37d7bd1cadb2a710591a68
Albrechtcolia <albrechtcolia@gmail.com>
2022-11-04 23:36:54 -0300

    Chapter 1

---
+++ source/chapter_1/exercise_1_15/fahrenheit_celsius.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+/**
+ * Exercise 1-15
+ * Rewrite the temperature conversion program of Section 1.2 to
+ * use a function for conversion.
+ */
+
+float fahrenheit_celsius (int fahrenheit);
+
+int main (int argc, char *argv[])
+{
+    int fahr;
+    int lower, upper, step;
+
+    lower = 0;      // Lower limit.
+    upper = 300;    // Upper limit.
+    step  = 20;     // Step size.
+    
+    printf("Fahrenheit\tCelsius\n");
+    printf("---------------------------\n");
+
+    for (fahr = lower; fahr <= upper; fahr += step)
+        printf("%3d\t\t%6.2f\n", fahr, fahrenheit_celsius(fahr));
+
+    return 0;
+}
+
+float fahrenheit_celsius (int fahrenheit) {
+    float celsius;
+    celsius = ((5.0 / 9.0) * (fahrenheit - 32));    
+
+    return(celsius);
+}
+
+
