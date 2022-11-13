d20b3eb8ef79dcfdb0b27dfd860c4f31b6d4942c
Albrechtcolia <albrechtcolia@gmail.com>
2022-11-13 08:08:17 -0300

    Chapter 1

--- source/chapter_1/exercise_1_13/horizontal_histogram.c
+++ source/chapter_1/exercise_1_13/horizontal_histogram.c
@@ -1,34 +1,55 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Exercise 1-13
  * Write a program to print a histogram of the lengths of words in its input.
- * It is easy to draw the histogram with the bars horizontal; a vertical orientation
- * is more challenging.
+ * It is easy to draw the histogram with the bars horizontal;
+ * a vertical orientation is more challenging.
  */
 
-#define MAX_WORDS 10  
+#define MAX_WORDS 10
 
 int main (int argc, char *argv[])
 {
-    int c, prev, index;
-    int wl[MAX_WORDS];
+    char c, prev = ' ';
+    int  wl[MAX_WORDS];
+    int  index = 0;
 
     for (int i = 0; i < MAX_WORDS; i++)
         wl[i] = 0;
 
-    prev = index = 0;
-    while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\t' || c == '\n') {
-            if (prev != ' ' && prev != '\t' && prev != '\n')
-                index++;
-        }
-        else
-            wl[index]++;
+    printf("Input data (CTRL + D, stop input)\n");
+    printf("---------------------------------\n");
 
-        prev = c;
+    while ((c = getchar()) != EOF) {
+        if (c >= 0 && c <= 127) {
+            if (c == ' ' || c == '\t' || c == '\n') {
+                if (prev != ' ' && prev != '\t' && prev != '\n') {
+                    index++;
+
+                    if (index == MAX_WORDS) {
+                        printf("\nNo more than %d words is allowed.\n", MAX_WORDS);
+                        printf("---------------------------------\n");
+
+                        break;
+                    }
+                }
+            }
+            else
+                wl[index]++;
+
+            prev = c;
+        }
+        else {
+            printf("\nUnknown characters.\n");
+            exit(-1);
+        }
     }
+
+    printf("\nHorizontal histogram\n");
+    printf("---------------------------------\n");
 
     for (int i = 0; i < index; i++) {
         printf("| %2d | ", i+1);
