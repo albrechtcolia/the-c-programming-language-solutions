1f8fa095191aa48713d021f75fb3ac49880b6bc7
Albrechtcolia <albrechtcolia@gmail.com>
2023-02-16 23:31:32 -0300

    Chapter 1

--- source/chapter_1/exercise_1_01/hello_world.c
+++ source/chapter_1/exercise_1_01/hello_world.c
@@ -5,7 +5,7 @@
  * Exercise 1-1
  * Run the hello, world program on your system.
  * Experiment with leaving out parts of the program,
- * to see what error messages you get
+ * to see what error messages you get.
  */
 
 int main (int argc, char *argv[])
