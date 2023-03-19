20552cde1fb7f82df7d3e92e1bf8f39c88a41bf5
Albrechtcolia <albrechtcolia@gmail.com>
2023-03-18 23:41:33 -0300

    Chapter 1

--- source/chapter_1/exercise_1_02/escape_sequence.c
+++ source/chapter_1/exercise_1_02/escape_sequence.c
@@ -8,7 +8,7 @@
  */
 
 int main (int argc, char *argv[])
-{ 
+{
     printf("alert:                         '\a'\n");
     printf("backspace:                     'backspace\b'\n");
     printf("formfeed:                      'formfeed\f'\n");
