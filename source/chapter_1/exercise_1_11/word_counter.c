7b26de78fadaf90015fb932e758d172099b3a82f
Albrechtcolia <albrechtcolia@gmail.com>
2023-03-22 23:48:07 -0400

    Chapter 1

--- source/chapter_1/exercise_1_11/word_counter.c
+++ source/chapter_1/exercise_1_11/word_counter.c
@@ -32,7 +32,7 @@ int main (int argc, char *argv[])
         else if (state == OUT) {
             state = IN;
             ++nw;
-        }    
+        }
     }
 
     printf("\n---------------------------\n");
