a32901b495c69ac8e102144c1ea5ffaeb6f3e960
Albrechtcolia <albrechtcolia@gmail.com>
2023-04-18 23:29:02 -0300

    Chapter 1

--- source/chapter_1/exercise_1_12/copy_one_word_per_line.c
+++ source/chapter_1/exercise_1_12/copy_one_word_per_line.c
@@ -15,7 +15,7 @@ int main (int argc, char *argv[])
             if (prev != ' ' && prev != '\t' && prev != '\n')
                 putchar('\n');
         }
-        else 
+        else
             putchar(c);
 
         prev = c;
